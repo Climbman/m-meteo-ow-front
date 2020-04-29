7ac328aa24f8e2073d0684f91814ff75414a8b17
Climbman <laurynas.kr@hotmail.com>
2020-04-29 23:51:08 +0300

    some work on login page style

--- templates/login.php
+++ templates/login.php
@@ -7,11 +7,11 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
 <html>
 <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" type="text/css" href="css/main.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link rel="stylesheet" type="text/css" href="css/main.css?<?= time();?>">
 <script type="text/javascript">
-    document.addEventListener('load', () => {
+    window.addEventListener('DOMContentLoaded', () => {
         document.body.addEventListener('keyup', event => {
-            console.log(event.keyCode);
             if (event.keyCode === 13) {
                 document.getElementById('login_form').submit();
             }
@@ -20,13 +20,13 @@ if (basename(__FILE__) == basename($_SERVER["SCRIPT_FILENAME"])) {
 </script>
 </head>
 <body class="login-body" id="login_body">
-<div class="login-form-holder transition03" id="login_form_holder">
-<form class="login-form" id="login_form" action="index.php" method="post">
-    <div class="heading1">Prisijungti</div>
+<div class="login-form-holder" id="login_form_holder">
+<form class="login-form transition03" id="login_form" action="index.php" method="post">
+    <div class="heading1"><code>m-f-w</code></div>
     <input type="hidden" name="login" value="" readonly>
-    <input class="login-txt-field transition03" type="text" name="user" placeholder="Vartotojas" required>
-    <input class="login-txt-field transition03" type="password" name="pass" placeholder="Slaptažodis" required>
-    <div class="login-button transition03" id="login_button" onclick="document.getElementById('login_form').submit();">Įeiti...</div>
+    <input class="login-txt-field transition03" type="text" name="user" autofocus autocomplete="off" placeholder="User" required>
+    <input class="login-txt-field transition03" type="password" name="pass" placeholder="Password" required>
+    <div class="login-button transition03" id="login_button" onclick="document.getElementById('login_form').submit();">login</div>
     
 </form>
 </div>
